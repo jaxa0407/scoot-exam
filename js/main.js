04117b612490393cd8eff7e959b808c0f17fa3dc
jaxa0407 <khabibjonov.j@student-ciu.uz>
2022-06-23 16:34:17 +0300

    faq section

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ var elBtn = document.querySelector(".header__menu-btn");
 elBtn.addEventListener("click", ( )=>{
     elHeader.classList.toggle("active")
 })
+
+var elFaq = document.querySelector(".faq__inner-wrap");
+var elButton = document.querySelector(".faq__qiestion-btn")
+
+elButton = addEventListener("click", ( )=>{
+    elFaq.classList.toggle("faq__open")
+})
